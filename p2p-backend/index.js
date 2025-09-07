1a1dacc1442f3104de0cc7e491e2084cd95b13c2
Wesley Waka <wesleywaka2@gmail.com>
2025-09-07 10:05:44 +0300

    update cors for client origin

--- p2p-backend/index.js
+++ p2p-backend/index.js
@@ -38,7 +38,7 @@ const io = new Server(server, {
 
 // Configure CORS
 app.use(cors({
-  origin: '*',
+  origin: process.env.CLIENT_ORIGIN || '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
